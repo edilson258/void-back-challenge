91430596d3cf550b6edcf7b40c487f59bc8e2ede
edilson258 <dev.258.edilson@gmail.com>
2025-08-30 20:23:31 +0200

    feat(domain): add campaign repository

---
+++ src/domain/repositories/campaign.repository.ts
@@ -0,0 +1,8 @@
+import type { DemoError } from "../../error";
+import type { Option } from "../../utils/option";
+import type { Result } from "../../utils/result";
+import type { CampaignEntity } from "../entities/campaign.entity";
+
+export interface CampaignRepository {
+  save: (compaign: CampaignEntity) => Promise<Result<void, DemoError>>;
+}
