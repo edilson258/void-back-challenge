608903fa94efe43e942a01cc3d8e38625a930605
edilson258 <dev.258.edilson@gmail.com>
2025-08-30 20:23:05 +0200

    feat(domain): add campaign entity

---
+++ src/domain/entities/campaign.entity.ts
@@ -0,0 +1,9 @@
+export class CampaignEntity {
+  constructor(
+    public readonly id: string,
+    public readonly name: string,
+    public readonly companyId: string,
+    public readonly startedAt: Date,
+    public readonly finishedAt: Date,
+  ) {}
+}
